35cc142a9175f589c9c25db73ba78531be3c133e
Jameson <jmerkow@gmail.com>
2022-03-17 14:15:49 -0700

    add copyright to py files (#17)
    * add copyright to py files
    * fix lint

--- src/model_drift/drift/unify.py
+++ src/model_drift/drift/unify.py
@@ -1,3 +1,7 @@
+#  ------------------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+#  ------------------------------------------------------------------------------------------
 import numpy as np
 import pandas as pd
 from sklearn.feature_selection import mutual_info_classif
